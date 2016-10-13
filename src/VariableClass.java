03f78de3134d21c7806bf29f61e94d8a4f6f2150
Mihkel1996 <mihkeljohannes.paimla@gmail.com>
2016-10-13 15:53:03 +0300

    Update VariableClass.java

--- src/VariableClass.java
+++ src/VariableClass.java
@@ -5,7 +5,7 @@ public class VariableClass {
 		//1. Kahe muutuja korrutamine, jagamine (võib proovida ka liitmist ja lahutamist)
 		int a = 10;
 		int b = 4;
-		int d = a + b
+		int d = a + b;
 		int c = a % b;
 //		boolean isEven = a % 2==
 //		System.out.println(c);
